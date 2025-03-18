8d9a3833d490890e8bcaa31374034542b3512daa
DanailaRoxana <danailaroxana2002@gmail.com>
2025-03-18 16:28:23 +0200

    organize files in folders

--- i2c_uvc/i2c_trans.sv
+++ i2c_uvc/i2c_trans.sv
@@ -9,7 +9,6 @@ class i2c_trans #(I2C_AW=7,I2C_DW=8) extends uvm_sequence_item;
   rand bit [I2C_DW-1:0]     data [$:255];   
   rand i2c_resp_kind_t      addr_resp   ;   
   rand i2c_resp_kind_t      data_resp   ; 
-  rand i2c_trans_end_kind_t trans_end   ;
   
   `uvm_object_utils_begin(i2c_trans)
     `uvm_field_int(addr, UVM_ALL_ON)
@@ -18,7 +17,6 @@ class i2c_trans #(I2C_AW=7,I2C_DW=8) extends uvm_sequence_item;
     `uvm_field_queue_int(data, UVM_ALL_ON)
     `uvm_field_enum(i2c_resp_kind_t, addr_resp, UVM_ALL_ON)
     `uvm_field_enum(i2c_resp_kind_t, data_resp, UVM_ALL_ON)
-    `uvm_field_enum(i2c_trans_end_kind_t, trans_end, UVM_ALL_ON) 
   `uvm_object_utils_end
 
   constraint data_order_c {solve nr_words before data;}
