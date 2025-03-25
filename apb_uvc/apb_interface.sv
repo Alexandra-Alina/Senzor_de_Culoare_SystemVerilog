c10a0baad0d675be577c7391f9dc9b773bda75ae
Alina <alina.dumitrica22@gmail.com>
2025-03-25 10:29:36 +0200

    feat: add assertions

--- apb_uvc/apb_interface.sv
+++ apb_uvc/apb_interface.sv
@@ -38,6 +38,55 @@ interface apb_interface #(APB_AW=32,APB_DW=32) (input clk, input rst_n);
     input pslverr ;
   endclocking: mon_cb
 
+  initial forever begin
+    @(posedge penable);
+    @(negedge clk);
+    stable_assertion_enable = 1'b1;
+    wait(pready === 1'b1);
+    stable_assertion_enable = 1'b0;
+    @(posedge clk);
+  end
+  
+  // Assertions
+  // Signals are never X or Z
+  assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(paddr))
+    else $error("APB Interface: !!! paddr is unknown !!!");
+  assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(psel))
+    else $error("APB Interface: !!! psel is unknown !!!");
+  assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(penable))
+    else $error("APB Interface: !!! penable is unknown !!!");
+  assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(pwrite))
+    else $error("APB Interface: !!! pwrite is unknown !!!");
+  assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(pwdata))
+    else $error("APB Interface: !!! pwdata is unknown !!!");
+  assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(prdata))
+    else $error("APB Interface: !!! prdata is unknown !!!");
+  assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(pready))
+    else $error("APB Interface: !!! pready is unknown !!!");
+  assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(pslverr))
+    else $error("APB Interface: !!! pslverr is unknown !!!");
+
+  // Signals don't change before pready
+  assert property (@(clk) disable iff (!rst_n) stable_assertion_enable |-> $stable(paddr))
+    else $error("APB Interface: !!! paddr changed before pready !!!");
+  assert property (@(clk) disable iff (!rst_n) stable_assertion_enable |-> $stable(psel))
+    else $error("APB Interface: !!! psel changed before pready !!!");
+  assert property (@(clk) disable iff (!rst_n) stable_assertion_enable |-> $stable(penable))
+    else $error("APB Interface: !!! penable changed before pready !!!");
+  assert property (@(clk) disable iff (!rst_n) stable_assertion_enable |-> $stable(pwrite))
+    else $error("APB Interface: !!! pwrite changed before pready !!!");
+  assert property (@(clk) disable iff (!rst_n) stable_assertion_enable |-> $stable(prdata))
+    else $error("APB Interface: !!! prdata changed before pready !!!");
+  assert property (@(clk) disable iff (!rst_n) stable_assertion_enable |-> $stable(pwdata))
+    else $error("APB Interface: !!! pwdata changed before pready !!!");
+  assert property (@(clk) disable iff (!rst_n) stable_assertion_enable |-> $stable(pslverr))
+    else $error("APB Interface: !!! pslverr changed before pready !!!");
+
+  // penable doesn't come without psel
+  assert property (@(posedge clk) disable iff (!rst_n) penable |-> psel)
+  else $error("APB Interface: !!! penable came without psel !!!");
+
+
 endinterface:apb_interface
 
 `endif
