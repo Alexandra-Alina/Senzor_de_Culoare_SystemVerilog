7996b3da3e40f3132df93b323eee48919cb33e65
Alexandra-Alina <96944850+Alexandra-Alina@users.noreply.github.com>
2025-05-06 16:17:48 +0300

    Update i2c_interface.sv

--- i2c_uvc/i2c_interface.sv
+++ i2c_uvc/i2c_interface.sv
@@ -7,7 +7,8 @@ interface i2c_interface (input clk, input rst_n);
   triand scl; // serial clock
   triand sda; // serial data
   
-  reg stable_assertion_enable;
+  reg stable_assertion_enable; // set when SCL is HIGH
+  reg trans_enable           ; // set when a transfer happens
 
   // Clocking blocks
   // Slave 
@@ -24,33 +25,40 @@ interface i2c_interface (input clk, input rst_n);
 
   // Assertions
 
-    initial forever begin
+  initial forever begin
     @(posedge scl);
     //@(negedge clk);
     stable_assertion_enable = 1'b1;
-     @(negedge scl);
+    @(negedge scl);
     stable_assertion_enable = 1'b0;
     //@(posedge clk);
+  end
+
+  initial forever begin
+    @(negedge sda iff scl === 'b1);
+    trans_enable = 1'b1;
+    @(posedge sda iff scl === 'b1);
+    trans_enable = 1'b0;
   end
   
 
   // Signals are never X or Z
   assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(scl))
-    else $error("I2C Interface: !!! scl is unknown !!!");
+    else $error("I2C Interface: !!! SCL is unknown !!!");
   assert property (@(posedge clk) disable iff (!rst_n) !$isunknown(sda))
-    else $error("I2C Interface: !!! sda is unknown !!!");
+    else $error("I2C Interface: !!! SDA is unknown !!!");
 
   // SDA doesn't change when SCL is HIGH 
-
- assert property (@(clk) disable iff (!rst_n) stable_assertion_enable |-> $stable(sda))
+  assert property (@(clk) disable iff (!rst_n) stable_assertion_enable |-> $stable(sda))
     else $error("I2C Interface: !!! SDA changed when SCL high!!!");
 
+  // SCL makes a full cycle
+  assert property (@(posedge clk) disable iff (!rst_n) $fell(scl) |-> ##[0:10000] $rose(scl))
+    else $error("I2C Interface: !!! SCL cycle didn't finish !!!");
 
-  assert property (@(posedge clk) disable iff (!rst_n)
-   $fell(scl) |-> ##[0:10000] $rose(scl);
-    else $error("I2C Interface: !!! scl is unknown !!!");
-
-    //scl toggle intre start si stop
+  // SCL toggles only between START and STOP
+  assert property (@(clk) disable iff (!rst_n) !trans_enable |-> $stable(scl))
+    else $error("I2C Interface: !!! SCL changed before START!!!");
 
 endinterface:i2c_interface
 
